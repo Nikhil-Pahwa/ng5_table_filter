7f7ebe47da442fa77ff2a94d0da318ba6b5b77d5
Nikhil-Pahwa <nikhil.pahwa@nagarro.com>
2018-02-18 17:40:22 +0530

    [ADD] Added scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { BooksService } from './services/book.service';
+import { BookFilterPipe } from './shared/book-filter.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [BooksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/book.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+import { Book } from '../shared/book';
+
+@Injectable()
+export class BooksService {
+    constructor(private http: Http) {
+    }
+
+    /**
+     * Load books from the static books.json data, usually an API URL.
+     *
+     * @return {Observable<Book[]>} A list of books.
+     */
+    getBooks(): Observable<Book[]> {
+        return this.http.get('assets/json/books.json')
+            .map(res => res.json().data);
+    }
+}
